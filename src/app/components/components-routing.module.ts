e36d65df48ffb1e15b1ed3741f5e087c20eb008a
heidiliu2020 <liuheidi2020@gmail.com>
2021-04-10 14:43:34 +0800

    feat: 延遲載入模組 components

--- src/app/components/components-routing.module.ts
+++ src/app/components/components-routing.module.ts
@@ -1,8 +1,14 @@
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CardsComponent } from './cards/cards.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComponentsComponent } from './components.component';
 
-const routes: Routes = [{ path: '', component: ComponentsComponent }];
+const routes: Routes = [
+  { path: '', component: ComponentsComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: 'buttons', component: ButtonsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
