5e84e20d4e171a7fa7f010b85c9c49bb9bc581e5
heidiliu2020 <liuheidi2020@gmail.com>
2021-04-10 16:35:28 +0800

    feat: 實作 Layout 頁面並調整路由結構

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
@@ -7,14 +9,19 @@ import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: LayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
+      { path: 'dashboard', component: DashboardComponent },
 
-  // 箭頭涵式可知為 callback，並且是 Promise 要用 then 接住
-  { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
-  { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
+      // 箭頭涵式可知為 callback，並且是 Promise 要用 then 接住
+      { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
+      { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent}
 ];
 
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+  origClass = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.origClass = document.body.className;
+    document.body.className = 'bg-gradient-primary';
+  }
+
+  ngOnDestroy(): void {
+    document.body.className = this.origClass;
+  }
+
+}
