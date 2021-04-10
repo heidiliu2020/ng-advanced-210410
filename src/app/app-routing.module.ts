434ab3c2fc6afec47242ff73e7124c34e5939df0
heidiliu2020 <liuheidi2020@gmail.com>
2021-04-10 15:51:44 +0800

    feat: preloadingStrategy: PreloadAllModules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -20,6 +20,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     useHash: true,
+    relativeLinkResolution: 'corrected',
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
