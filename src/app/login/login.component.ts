3b34baa0396d32e22b73dafd3d9db0549d7fcca0
heidiliu2020 <liuheidi2020@gmail.com>
2021-04-17 11:47:21 +0800

    feat: 實作 ngSubmit 並將表單改為單向繫結

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
@@ -7,8 +8,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit, OnDestroy {
 
   data: any = {
-    email: '',
-    password: '',
+    email: 'user@example.com',
+    password: '1234abcd',
     isRememberMe: true
   };
 
@@ -19,6 +20,15 @@ export class LoginComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.origClass = document.body.className;
     document.body.className = 'bg-gradient-primary';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    if (form.valid) {
+      console.log('成功送出表單', form.value);
+    } else {
+      console.log('送出失敗，請填寫正確格式', form.value);
+    }
   }
 
   ngOnDestroy(): void {
