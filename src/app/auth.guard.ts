97d60875cbb0ea92fc2f6c5ebd87a743b6086b4f
heidiliu2020 <liuheidi2020@gmail.com>
2021-04-17 09:13:15 +0800

    fix: 修改註解

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -14,10 +14,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    // 只會出現 true/false，避免型別判別出錯
+    // !! 只會出現 true/false，避免型別判別出錯
     if (!!localStorage.getItem('token')) {
       return true;
     } else {
+      // 假如沒有 token 就直接導向 login
       return this.router.createUrlTree(['/login'], {
         queryParams: {
           returnUrl: state.url
